9342b186da576270d058ab7d52aaf094ecea2681
BenjiTam <95749110+BenjiTam@users.noreply.github.com>
2022-03-25 10:33:52 +0100

    UP

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
 /*-- Scroll Top --*/
+
+const monBouton = document.getElementById("scrollTop");
+
+window.onscroll = function(){
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) { 
+        monBouton.style.display = "block"
+    } else {
+        monBouton.style.display = "none"
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0; 
+}
